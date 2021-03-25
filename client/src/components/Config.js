8574a15763f0f1ea7df44217a7b768a2d70b00d9
RoyalOttoc <jong.kim8706@gmail.com>
2021-03-26 05:32:34 +1100

    add landing page with main image

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,2 +1,6 @@
 //SERVER ROUTES
 export const USER_SERVER = '/api/users';
+
+export const API_URL = 'https://api.themoviedb.org/3'
+export const API_KEY = '4a01f12f99b412e7362029de0c2ca245'
+export const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p'
